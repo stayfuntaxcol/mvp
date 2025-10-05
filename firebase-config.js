308ebda811a3e4803e289d22b2e9fea7daea8154
stayfuntaxcol <stephan.tollenaar@gmail.com>
2025-10-05 12:43:57 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
-
+// Replace with your own Firebase config
+export const firebaseConfig = {  
+  apiKey: "AIzaSyAS3b0hiLZIg1ZvkFw1mYO754uT-ropTy0",
+  authDomain: "employee-award-nominations.firebaseapp.com",
+  projectId: "employee-award-nominations",
+  storageBucket: "employee-award-nominations.firebasestorage.app",
+  messagingSenderId: "980948488527",
+  appId: "1:980948488527:web:22f7d2c671e48eaca5b53a",
+  measurementId: "G-2490NHQ7CT"
+};
